73451c0803f8f9b9c6f1b2d9c94ab249d20a66b2
rapucadyil <rahuljoseph11@gmail.com>
2018-12-11 14:11:21 +0100

    started up with RWD basic skeleton

---
+++ scripts/styleswitcher.js
@@ -0,0 +1,12 @@
+/* Just a debug function to test this script out delete in prod */
+function debug(btn_name) {
+    console.log("Clicking button: " + btn_name);
+}
+
+function switchToDark() {
+    
+}
+
+function switchToLight() {
+    
+}
