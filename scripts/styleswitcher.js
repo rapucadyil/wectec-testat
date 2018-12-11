f299608dbe8818e961c2d21c7942b8957dd7e3ca
rapucadyil <rahuljoseph11@gmail.com>
2018-12-11 18:30:05 +0100

    style swithcer working

--- scripts/styleswitcher.js
+++ scripts/styleswitcher.js
@@ -3,10 +3,14 @@ function debug(btn_name) {
     console.log("Clicking button: " + btn_name);
 }
 
-function switchToDark() {
-    
+function swapStyleSheet(sheet_name) {
+    document.getElementById("style").setAttribute("href", sheet_name);
 }
 
-function switchToLight() {
-    
-}
+/*function initiate() {
+    var style1 = document.getElementById("stylesheet1");
+    var style2 = document.getElementById("stylesheet2");
+
+    style1.onclick = swapStyleSheet("alternate.css");
+    style2.onclick = swapStyleSheet("style.css");
+}*/
